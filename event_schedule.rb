babc11b132d955e2b0eeef870e6f4517a89fbf91
breeze-yang <breeze@deshpro.com>
2019-02-11 17:07:47 +0800

    完成赛程

---
+++ event_schedule.rb
@@ -0,0 +1,11 @@
+class EventSchedule < ApplicationRecord
+  mount_uploader :schedule_pdf, PdfUploader
+
+  belongs_to :main_event
+
+  after_initialize do
+    self.begin_time ||= Time.current
+    self.reg_open ||= Time.current
+    self.reg_close ||= Time.current
+  end
+end
--- main_event.rb
+++ main_event.rb
@@ -2,6 +2,8 @@ class MainEvent < ApplicationRecord
   include Publishable
   mount_uploader :logo, ImageUploader
 
+  has_many :event_schedules
+
   after_initialize do
     self.begin_time ||= Time.current
     self.end_time ||= Time.current
