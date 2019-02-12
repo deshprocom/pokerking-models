e2524c72db1ff76962ee0eb01e225b8a35bb061c
breeze-yang <breeze@deshpro.com>
2019-02-12 18:55:49 +0800

    完善models

--- event_schedule.rb
+++ event_schedule.rb
@@ -2,7 +2,6 @@ class EventSchedule < ApplicationRecord
   mount_uploader :schedule_pdf, PdfUploader
 
   belongs_to :main_event
-
   after_initialize do
     self.begin_time ||= Time.current
     self.reg_open ||= Time.current
--- main_event.rb
+++ main_event.rb
@@ -2,7 +2,10 @@ class MainEvent < ApplicationRecord
   include Publishable
   mount_uploader :logo, ImageUploader
 
-  has_many :event_schedules
+  has_many :event_schedules, -> { order(begin_time: :asc) }
+
+  scope :begin_asc, -> { order(begin_time: :asc) }
+  scope :begin_desc, -> { order(begin_time: :desc) }
 
   after_initialize do
     self.begin_time ||= Time.current
