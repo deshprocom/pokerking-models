a33ddc76103282a66490ce8f3ca3d16459399a2e
Ricky <rickycxj@gmail.com>
2019-03-17 18:39:01 +0800

    增加排序功能

--- cash_game.rb
+++ cash_game.rb
@@ -1,7 +1,7 @@
 class CashGame < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :cash_queues, dependent: :destroy
-  scope :position_desc, -> { order(position: :desc) }
+  scope :position_desc, -> { order(position: :desc).order(created_at: :desc) }
 
   before_create do
     self.position = CashGame.position_desc.first&.position.to_f + 100000
--- cash_queue_member.rb
+++ cash_queue_member.rb
@@ -2,7 +2,8 @@ class CashQueueMember < ApplicationRecord
   belongs_to :cash_queue, counter_cache: true
   include Cancelable
   validates :nickname, presence: true, uniqueness:true
-  scope :position_desc, -> { order(position: :desc) }
+  scope :position_desc, -> { order(position: :desc).order(created_at: :desc) }
+  scope :position_asc, -> { order(position: :asc).order(created_at: :asc) }
 
   before_create do
     self.position = CashQueueMember.position_desc.first&.position.to_f + 100000
