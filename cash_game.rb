2eece4fd5e55fb779035cb5eceecbc344261f05e
Ricky <rickycxj@gmail.com>
2019-03-17 18:30:12 +0800

    增加排序

--- cash_game.rb
+++ cash_game.rb
@@ -1,4 +1,9 @@
 class CashGame < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :cash_queues, dependent: :destroy
+  scope :position_desc, -> { order(position: :desc) }
+
+  before_create do
+    self.position = CashGame.position_desc.first&.position.to_f + 100000
+  end
 end
--- cash_queue_member.rb
+++ cash_queue_member.rb
@@ -2,4 +2,9 @@ class CashQueueMember < ApplicationRecord
   belongs_to :cash_queue, counter_cache: true
   include Cancelable
   validates :nickname, presence: true, uniqueness:true
+  scope :position_desc, -> { order(position: :desc) }
+
+  before_create do
+    self.position = CashQueueMember.position_desc.first&.position.to_f + 100000
+  end
 end
