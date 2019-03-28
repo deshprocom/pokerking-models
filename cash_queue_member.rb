ab63d15589880d704bb6a7b618eb69dd5215dc25
Ricky <rickycxj@gmail.com>
2019-03-28 15:14:54 +0800

    快捷排队

--- cash_queue_member.rb
+++ cash_queue_member.rb
@@ -1,7 +1,7 @@
 class CashQueueMember < ApplicationRecord
   belongs_to :cash_queue, counter_cache: true
   include Cancelable
-  validates :nickname, presence: true, uniqueness:true
+  # validates :nickname, presence: true, uniqueness:true
   scope :position_desc, -> { order(position: :desc).order(created_at: :desc) }
   scope :position_asc, -> { order(position: :asc).order(created_at: :asc) }
 
