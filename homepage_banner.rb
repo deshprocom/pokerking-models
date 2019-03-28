1d24c3ee3f4992ada0b68df77c171181d8861c60
Ricky <rickycxj@gmail.com>
2019-03-28 16:27:40 +0800

    增加繁体图片

--- homepage_banner.rb
+++ homepage_banner.rb
@@ -3,6 +3,7 @@ class HomepageBanner < ApplicationRecord
   validates :source_type, presence: true
   validates :source_id, presence: true
   validates :image, presence: true, if: :new_record?
+  scope :position_desc, -> { order(position: :desc).order(id: :desc) }
 
   def source
     @source ||= source_id && source_type.classify.safe_constantize.find(source_id)
