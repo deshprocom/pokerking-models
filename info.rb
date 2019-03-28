51a6bdbcd8eef43b8af78205482fe65006e254f9
Ricky <rickycxj@gmail.com>
2019-03-28 11:00:04 +0800

    资讯增加热门选项

--- info.rb
+++ info.rb
@@ -6,4 +6,17 @@ class Info < ApplicationRecord
   after_initialize do
     self.created_at ||= Time.current
   end
+
+  scope :position_desc, -> { order(position: :desc) }
+  before_create do
+    self.position = Info.position_desc.first&.position.to_f + 100000
+  end
+
+  def hot!
+    update(hot: true)
+  end
+
+  def unhot!
+    update(hot: false)
+  end
 end
