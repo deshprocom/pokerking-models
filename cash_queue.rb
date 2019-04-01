83aa379dcc68c95943d5fdf6b6b8ddb3d48219ba
Ricky <rickycxj@gmail.com>
2019-04-01 18:12:22 +0800

    排队顺序问题

--- cash_queue.rb
+++ cash_queue.rb
@@ -1,4 +1,9 @@
 class CashQueue < ApplicationRecord
   belongs_to :cash_game
   has_many :cash_queue_members, dependent: :destroy
+
+  # 当天排队的人数
+  def current_day_members
+    cash_queue_members.current_day.count
+  end
 end
--- cash_queue_member.rb
+++ cash_queue_member.rb
@@ -2,8 +2,9 @@ class CashQueueMember < ApplicationRecord
   belongs_to :cash_queue, counter_cache: true
   include Cancelable
   # validates :nickname, presence: true, uniqueness:true
-  scope :position_desc, -> { order(position: :desc).order(created_at: :desc) }
-  scope :position_asc, -> { order(position: :asc).order(created_at: :asc) }
+  scope :position_desc, -> { order(position: :desc).current_day }
+  scope :position_asc, -> { order(position: :asc).current_day }
+  scope :current_day, -> {where('created_at >= ? and created_at <= ?', Date.current.beginning_of_day, Date.current.end_of_day)}
 
   before_create do
     self.position = CashQueueMember.position_desc.first&.position.to_f + 100000
