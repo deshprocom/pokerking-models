c6ed010b1c942c31640c41d0f955f67ca56892ea
Ricky <rickycxj@gmail.com>
2019-04-02 11:44:41 +0800

    更改排队人数显示问题

--- cash_queue.rb
+++ cash_queue.rb
@@ -1,9 +1,4 @@
 class CashQueue < ApplicationRecord
   belongs_to :cash_game
   has_many :cash_queue_members, dependent: :destroy
-
-  # 当天排队的人数
-  def current_day_members
-    cash_queue_members.count
-  end
 end
