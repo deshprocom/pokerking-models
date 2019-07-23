9ce373cabf6f0007665bc128dbab8ce486fad21f
ricky <ricky@rickydeiMac.local>
2019-07-23 13:58:44 +0800

    添加排序

--- cash_queue.rb
+++ cash_queue.rb
@@ -2,8 +2,18 @@ class CashQueue < ApplicationRecord
   mount_uploader :navigation, ImageUploader
   belongs_to :cash_game
   has_many :cash_queue_members, dependent: :destroy
+  scope :position_desc, -> { order(position: :desc) }
+  scope :position_asc, -> { order(position: :asc) }
+
+  before_create do
+    self.position = CashQueue.position_desc.first&.position.to_f + 100000
+  end
 
   def table_nums
     table_no.split(',').count
+  end
+
+  def blind_info
+    straddle.to_i.zero? ? "#{small_blind}/#{big_blind}" : "#{small_blind}/#{big_blind}/#{straddle}"
   end
 end
