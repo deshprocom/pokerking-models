fd8512985ee3b36ea05d34d9e1700a74761dec1a
ricky <ricky@rickydeiMac.local>
2019-08-08 17:12:40 +0800

    修改 notify

--- notification.rb
+++ notification.rb
@@ -10,7 +10,7 @@ class Notification < ApplicationRecord
   def self.create_queue_notify(user, cash_queue)
     cash_game_name = cash_queue.cash_game.name # 扑克房名字
     apply_index = cash_queue.current_user_index(user) # 报名后的序号
-    content = "您所参加的：#{cash_game_name}盲注#{cash_queue.blind_info}前面还有#{apply_index}位在排队等候！请及时赶往现场办理入场手续！"
+    content = "您所参加的：#{cash_game_name}盲注#{cash_queue.blind_info}NLH前面还有#{apply_index}位在排队等候！请及时赶往现场办理入场手续！"
     create(user: user,
            notify_type: 'scan_apply',
            title: '排队进程通知',
@@ -20,7 +20,7 @@ class Notification < ApplicationRecord
 
   def self.cancel_queue_notify(user, cash_queue)
     cash_game_name = cash_queue.cash_game.name # 扑克房名字
-    content = "取消：#{cash_game_name}盲注#{cash_queue.blind_info}报名成功！"
+    content = "您所参加的#{cash_game_name}盲注#{cash_queue.blind_info}NLH已取消排队等候！"
     create(user: user,
            notify_type: 'cancel_apply',
            title: '取消排队通知',
