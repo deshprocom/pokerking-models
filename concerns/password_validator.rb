46b4d7e11367ff8e2be1cfe9e23d4c8209b9b325
ricky <ricky@rickydeiMac.local>
2019-09-19 11:14:39 +0800

    增强密码验证

--- concerns/password_validator.rb
+++ concerns/password_validator.rb
@@ -1,13 +1,12 @@
 # 用户密码验证器
 module PasswordValidator
-  # 有效的密码格式满足的条件
-  # 验证是否是md5加密后的
   PWD_VALID_FORMAT_REGEX = /^[a-fA-F0-9]{32}$/
   extend ActiveSupport::Concern
 
   module ClassMethods
     def pwd_valid?(pwd)
-      pwd.to_s.match? PasswordValidator::PWD_VALID_FORMAT_REGEX
+      # 有效的md5格式 并且密码不能为空
+      (pwd.to_s.match? PasswordValidator::PWD_VALID_FORMAT_REGEX) && !pwd.eql?('d41d8cd98f00b204e9800998ecf8427e')
     end
   end
 end
