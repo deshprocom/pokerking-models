d25ec7b3d1e3f358155cb62cddcac6fb46f6a1ef
ricky <ricky@rickydeiMac.local>
2019-10-29 15:28:21 +0800

    增加实名认证的接口

--- user_extra.rb
+++ user_extra.rb
@@ -1,4 +1,10 @@
 class UserExtra < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  mount_uploader :img_front, ImageUploader
   belongs_to :user, optional: true
+
+  def image_path
+    return '' if img_front.url.nil?
+
+    img_front.url
+  end
 end
