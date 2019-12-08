3354da402945f1b1340335eb6a1ba6447ae0fe1c
ricky <ricky@rickydeiMac.local>
2019-12-08 17:25:22 +0800

    新增日志历史数据

--- cash_game.rb
+++ cash_game.rb
@@ -4,6 +4,7 @@ class CashGame < ApplicationRecord
   mount_uploader :image_complex, ImageUploader
   has_many :cash_queues, dependent: :destroy
   scope :position_desc, -> { order(position: :desc).order(created_at: :desc) }
+  has_paper_trail
 
   before_create do
     self.position = CashGame.position_desc.first&.position.to_f + 100000
